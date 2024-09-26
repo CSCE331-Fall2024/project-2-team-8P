d730bff570ca13561649ba9b51041858dd59c8db
Bradley James <bj2772798@tamu.edu>
2024-09-26 04:08:46 +0000

    Assigned j random meals for day i, fixed values to be closer to required total sales, and started implementing logic for peak days

--- fill.py
+++ fill.py
@@ -1,33 +1,34 @@
 import numpy as np
 import random
-# import psycopg2, psycopg2.sql as sql
-# from templates import create_tables, insert_tables
+import psycopg2, psycopg2.sql as sql
+from templates import create_tables, insert_tables
 import math
 # Python script to dynamically populate sql database
 # Dictionary for menu items
 
 menu_items = {
+    # Added Arbitrary values, can change later if needed
     "sides" : 
     {
-        "Chow Mein" : 2.5,
-        "Fried Rice" : 2.5,
-        "White Steamed Rice" : 2.5,
-        "Super Greens" : 2.5
+        "Chow Mein" : 3,
+        "Fried Rice" : 3,
+        "White Steamed Rice" : 3,
+        "Super Greens" : 3
     },
     "entrees" : 
     {
-       "Hot Ones Blazing Bourbon Chicken" : 2.5,
-        "The Original Orange Chicken" : 2.5,
-        "Black Pepper Sirloin Steak" : 2.5,
-        "Honey Walnut Shrimp" : 2.5,
-        "Grilled Teriyaki Shrimp" : 2.5,
-        "Broccoli Beef" : 2.5,
-        "Kung Pao Chicken" : 2.5,
-        "Honey Sesame Chicken Breast" : 2.5,
-        "Beijing Beef" : 2.5,
-        "Black Pepper Chicken" : 2.5,
-        "Cream Cheese Rangoon" : 2.5,
-        "Chicken Egg Roll" : 2.5 
+       "Hot Ones Blazing Bourbon Chicken" : 3.25,
+        "The Original Orange Chicken" : 3.25,
+        "Black Pepper Sirloin Steak" : 3.5,
+        "Honey Walnut Shrimp" : 3.5,
+        "Grilled Teriyaki Shrimp" : 3.25,
+        "Broccoli Beef" : 3.25,
+        "Kung Pao Chicken" : 3.25,
+        "Honey Sesame Chicken Breast" : 3.25,
+        "Beijing Beef" : 3.25,
+        "Black Pepper Chicken" : 3.25,
+        "Cream Cheese Rangoon" : 2,
+        "Chicken Egg Roll" : 2 
     },
     
     "drinks" : 
@@ -54,15 +55,54 @@ dist_orders = np.ceil(dist_day/10)
 # Assign an hour to that order 
 # Make 
 dist_hour = np.random.randint(1, 13, int(np.sum(dist_orders)))
-print(dist_hour)
+# print(dist_hour)
     
-# side = random.choice(list(menu_items))
-side = random.choice(list(menu_items["sides"]))
-entree = random.choice(list(menu_items["entrees"]))
-drink = random.choice(list(menu_items["drinks"]))
-print(side)
-print(entree)
-print(drink)
+sum_of_meals = 0
+meal_option = 0
+counter = 0
+
+# Assigns j random orders for i days
+for i in dist_orders:
+    for j in range(0, int(i)):
+        meal_option = random.randint(1, 3)
+        if(meal_option == 1): # Bowl
+            side = random.choice(list(menu_items["sides"]))
+            sum_of_meals += menu_items["sides"][side]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            drink = random.choice(list(menu_items["drinks"]))
+            sum_of_meals += menu_items["drinks"][drink]
+        elif(meal_option == 2): # Plate
+            side = random.choice(list(menu_items["sides"]))
+            sum_of_meals += menu_items["sides"][side]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            drink = random.choice(list(menu_items["drinks"]))
+            sum_of_meals += menu_items["drinks"][drink]
+        else: # Bigger Plate
+            side = random.choice(list(menu_items["sides"]))
+            sum_of_meals += menu_items["sides"][side]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            entree = random.choice(list(menu_items["entrees"]))
+            sum_of_meals += menu_items["entrees"][entree]
+            drink = random.choice(list(menu_items["drinks"]))
+            sum_of_meals += menu_items["drinks"][drink]
+        counter += 1
+
+peak_day_1 = random.randint(0, len(dist_orders))
+peak_day_2 = random.randint(0, len(dist_orders))
+print("Peak Day 1: ", peak_day_1)
+print("Peak Day 2: ", peak_day_2)
+print("Sum of all meals over all days: ", sum_of_meals)
+print("Number of orders: ", counter)
+# print(side)
+# print(entree)
+# print(drink)
 # Pick menu items for each order randomly
 # Assign cashiers to orders randomly
 
