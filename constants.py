0d55bf0a4520452b4851eaacc0bd242ea24cf531
Soham Nagawanshi <ndsoham@tamu.edu>
2024-09-27 11:02:41 -0500

    Added logic to populate menu items

---
+++ constants.py
@@ -0,0 +1,46 @@
+# Employee Data Structure (isManager, Name)
+employees = [
+    (True, "Ash"),
+    (True, "Pikachu"),
+    (False, "Charizard"),
+    (False, "Greninja"),
+    (False, "Jigglypuff"),
+    (False, "Snorlax"),
+    (False, "Mewtwo"),
+    (False, "Gengar"),
+    (False, "Magikarp"),
+    (False, "Ditto")
+]
+
+# Menu Item Datastructure (price, availableStock, itemName)
+base_menu_items = [
+    (2.1, 451, 'Dr. Pepper'), 
+    (2.1, 835, 'Aquafina'), 
+    (2.1, 701, 'Sweet Tea'), 
+    (2.1, 529, 'Pepsi'), 
+    (4.4, 382, 'Chow Mein'), 
+    (4.4, 980, 'Fried Rice'), 
+    (4.4, 453, 'White Steamed Rice'), 
+    (4.4, 647, 'Super Greens'), 
+    (5.2, 912, 'Hot Ones Blazing Bourbon Chicken'), 
+    (5.2, 365, 'The Original Orange Chicken'), 
+    (6.7, 761, 'Black Pepper Sirloin Steak'), 
+    (6.7, 604, 'Honey Walnut Shrimp'), 
+    (5.2, 536, 'Grilled Teriyaki Shrimp'), 
+    (5.2, 245, 'Broccoli Beef'), 
+    (5.2, 412, 'Kung Pao Chicken'), 
+    (5.2, 611, 'Honey Sesame Chicken Breast'), 
+    (5.2, 133, 'Beijing Beef'), 
+    (5.2, 763, 'Black Pepper Chicken'), 
+    (2.0, 279, 'Cream Cheese Rangoon'), 
+    (2.0, 345, 'Chicken Egg Roll')
+]
+# scale factor 0.86
+plate_menu_items = base_menu_items[3:17]
+plate_menu_items = []
+
+bowl_menu_items = {
+    
+}
+
+
--- fill.py
+++ fill.py
@@ -2,129 +2,101 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 import psycopg2, psycopg2.sql as sql
-from templates import create_tables, insert_tables
+from templates import create_tables, insert_tables, i_employee_table, i_menu_item_table
+from constants import employees, base_menu_items
 import math
 # Python script to dynamically populate sql database
 # Dictionary for menu items
 
-menu_items = {
-    # Added Arbitrary values, can change later if needed
-    "sides" : 
-    {
-        "Chow Mein" : 3,
-        "Fried Rice" : 3,
-        "White Steamed Rice" : 3,
-        "Super Greens" : 3
-    },
-    "entrees" : 
-    {
-       "Hot Ones Blazing Bourbon Chicken" : 3.25,
-        "The Original Orange Chicken" : 3.25,
-        "Black Pepper Sirloin Steak" : 3.5,
-        "Honey Walnut Shrimp" : 3.5,
-        "Grilled Teriyaki Shrimp" : 3.25,
-        "Broccoli Beef" : 3.25,
-        "Kung Pao Chicken" : 3.25,
-        "Honey Sesame Chicken Breast" : 3.25,
-        "Beijing Beef" : 3.25,
-        "Black Pepper Chicken" : 3.25,
-        
-    },
-    "appetizers" : {
-        "Cream Cheese Rangoon" : 2,
-        "Chicken Egg Roll" : 2 
-    },
-
-    "drinks" : 
-    {
-        "Dr. Pepper" : 1,
-        "Aquafina" : 1,
-        "Sweet Tea" : 1,
-        "Pepsi" : 1
-    }
-   
-}
-
 # Generate a random distribution for sales per day for 365 days
-np.random.seed(10)
-dist_day = np.random.normal(2733.90, 100.65, (365, 1))
-sum_sales = np.sum(dist_day)
-sum_sales = round(sum_sales, 2)
-print("Yearly Sales Sum: ", sum_sales)
+# np.random.seed(10)
+# dist_day = np.random.normal(2733.90, 100.65, (365, 1))
+# sum_sales = np.sum(dist_day)
+# sum_sales = round(sum_sales, 2)
+# print("Yearly Sales Sum: ", sum_sales)
 
 # Generate a random number of orders for each day
-dist_orders = np.ceil(dist_day/10)
-sum_orders = sum(dist_orders)[0]
-print("Yearly Orders Sum: ", sum_orders)
+# dist_orders = np.ceil(dist_day/10)
+# sum_orders = sum(dist_orders)[0]
+# print("Yearly Orders Sum: ", sum_orders)
 
 # dist_total keeps track of all generated values
-dist_total = np.concatenate((dist_day, dist_orders), axis=1)
+# dist_total = np.concatenate((dist_day, dist_orders), axis=1)
 
 # Assign an hour to each order 
 # Make
-np.random.seed(10) 
-dist_hour = np.random.randint(1, 13, int(np.sum(dist_orders)))
+# np.random.seed(10) 
+# dist_hour = np.random.randint(1, 13, int(np.sum(dist_orders)))
 # print(dist_hour)
     
-sum_of_meals = 0
-meal_option = 0
-counter = 0
-sum_day = []
+# sum_of_meals = 0
+# meal_option = 0
+# counter = 0
+#  = []
 # Assigns j random orders for i days
 random.seed(10)
-for i in dist_orders:
-    sum_order = 0
-    for j in range(0, int(i)):
-        meal_option = random.randint(1, 5)
-        has_drink = random.randint(0, 1)
-        if(meal_option == 1): # Bowl
-            side = random.choice(list(menu_items["sides"]))
-            sum_order += menu_items["sides"][side]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-        elif(meal_option == 2): # Plate
-            side = random.choice(list(menu_items["sides"]))
-            sum_order += menu_items["sides"][side]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-        elif (meal_option == 3): # Bigger Plate
-            side = random.choice(list(menu_items["sides"]))
-            sum_order += menu_items["sides"][side]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-        elif (meal_option == 4): # A La Carte (Individual Entrees & Sides)
-            entree = random.choice(list(menu_items["entrees"]))
-            sum_order += menu_items["entrees"][entree]
-            drink = random.choice(list(menu_items["drinks"]))
-            sum_order += menu_items["drinks"][drink]
 
-        elif (meal_option == 5): # Appetizers
-            appetizer = random.choice(list(menu_items["appetizers"]))
-            sum_order += menu_items["appetizers"][appetizer]
+# Loop until each day's total has been fulfilled
+    # Pick a random order (Bowl, plate, bigger plate or individual menu item, hasdrink (bool), hasappetizer (bool))
+    # Assign a random hour to that order
+    # Assign a random cashier
+    # Put it in the database
+
+
+
+# for i in dist_orders:
+#     for j in range(0, int(i)):
+#         sum_order = 0
+#         meal_option = random.randint(1, 5)
+#         has_drink = random.randint(0, 1)
+#         if(meal_option == 1): # Bowl
+#             side = random.choice(list(menu_items["sides"]))
+#             sum_order += menu_items["sides"][side]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#         elif(meal_option == 2): # Plate
+#             side = random.choice(list(menu_items["sides"]))
+#             sum_order += menu_items["sides"][side]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#         elif (meal_option == 3): # Bigger Plate
+#             side = random.choice(list(menu_items["sides"]))
+#             sum_order += menu_items["sides"][side]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#         elif (meal_option == 4): # A La Carte (Individual Entrees & Sides)
+#             entree = random.choice(list(menu_items["entrees"]))
+#             sum_order += menu_items["entrees"][entree]
+#             drink = random.choice(list(menu_items["drinks"]))
+#             sum_order += menu_items["drinks"][drink]
+
+#         elif (meal_option == 5): # Appetizers
+#             appetizer = random.choice(list(menu_items["appetizers"]))
+#             sum_order += menu_items["appetizers"][appetizer]
             
-        if has_drink:
-                drink = random.choice(list(menu_items["drinks"]))
-                sum_order += menu_items["drinks"][drink]
-        counter += 1
-    sum_day.append(sum_order)
+#         if has_drink:
+#                 drink = random.choice(list(menu_items["drinks"]))
+#                 sum_order += menu_items["drinks"][drink]
+#         counter += 1
+#     sum_day.append(sum_order)
 
-xs = np.linspace(1, len(sum_day), num=len(sum_day))
-plt.plot(xs, sum_day)
-plt.show()
+# xs = np.linspace(1, len(sum_day), num=len(sum_day))
+# plt.plot(xs, sum_day)
+# plt.show()
 
-print(np.array(sum_day).reshape((len(sum_day), 1)))
+# print(np.array(sum_day).reshape((len(sum_day), 1)))
 
 # peak_day_1 = random.randint(0, len(dist_orders))
 # peak_day_2 = random.randint(0, len(dist_orders))
 # print("Peak Day 1: ", peak_day_1)
 # print("Peak Day 2: ", peak_day_2)
-print("Sum of all meals over all days: ", np.sum(sum_day))
+# print("Sum of all meals over all days: ", np.sum(sum_day))
 # print("Number of orders: ", counter)
 
 
@@ -143,30 +115,36 @@ print("Sum of all meals over all days: ", np.sum(sum_day))
 #     sum += dist_hour[i]
 # print(round(sum, 2))
 
-
-
 # connect to the local database
 
-# conn = psycopg2.connect(host="csce-315-db.engr.tamu.edu", dbname="team_8p_db", user="team_8p", password="Electabuzz8!")
-# cur = conn.cursor()
+print("Connecting to database")
+conn = psycopg2.connect(host="csce-315-db.engr.tamu.edu", dbname="team_8p_db", user="team_8p", password="Electabuzz8!")
+cur = conn.cursor()
 
-# for table in basetables:
+# Create all tables
+# for table in create_tables:
 #     cur.execute(table)
 
-# cur.execute(
-#     sql.SQL(insert_tables[0]),
-#     [False, "Ash"]
+# Insert items into employee table
+# for employee in employees:
+#     cur.execute(
+#         sql.SQL(i_employee_table),
+#         [employee[0], employee[1]]
 # )
 
-# cur.execute(
-#     sql.SQL(insert_tables[1]),
-#      [1, 3, 5, 7, 9]
-# )
+# Insert items into the menu items table
+for item in base_menu_items:
+    cur.execute(
+        sql.SQL(i_menu_item_table),
+        [item[0], item[1], item[2]]
+)
 
-# print("Done executing")
+# Insert items into menu item table
+
+print("Done executing")
 
 # commit changes and close the connection
-# conn.commit()
+conn.commit()
 
-# cur.close()
-# conn.close()
+cur.close()
+conn.close()
