b3343e758ea28e353e8db2f22bf46613566bed33
Shreyan Satheesh <s0901527@students.katyisd.org>
2024-10-09 09:14:57 -0500

    commented out menu types and adjusted layout accordingly

--- PandaExpressPOS/src/main/java/org/example/pandaexpresspos/HelloApplication.java
+++ PandaExpressPOS/src/main/java/org/example/pandaexpresspos/HelloApplication.java
@@ -13,9 +13,9 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/hello-view.fxml"));
         FXMLLoader CashierLoader = new FXMLLoader(getClass().getResource("fxml/cashier-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        //Scene cashierApp = new Scene(CashierLoader.load(), 1200, 800);
+        Scene cashierApp = new Scene(CashierLoader.load(), 1200, 800);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(cashierApp);
         stage.show();
 
     }
