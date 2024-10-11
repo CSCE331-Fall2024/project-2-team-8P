4b6d670633bd2f8f3a15f48f3fba1800036d97d7
Rkha21 <rkha02134@gmail.com>
2024-10-11 10:04:17 -0500

    Added Alert for invalid users and changed EmployeeType.ERROR logic

--- PandaExpressPOS/src/main/java/org/example/pandaexpresspos/controllers/LoginController.java
+++ PandaExpressPOS/src/main/java/org/example/pandaexpresspos/controllers/LoginController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -62,8 +63,8 @@ public class LoginController {
                 break;
             default:
                 // TODO: display UI for Error
-                System.out.println("Employee does not exist, exiting...");
-                System.exit(1);
+                System.out.println("Employee does not exist.");
+
         }
 
         // Create a new scene and set it to the stage
@@ -80,6 +81,7 @@ public class LoginController {
     private EmployeeType getEmployeeType(String username) {
         setCurrentUser(username);
         if (currentUser == null) {
+            showAlert("Invalid Username", "The username you entered does not exist.");
             return EmployeeType.ERROR;
         }
 
@@ -89,5 +91,11 @@ public class LoginController {
             return EmployeeType.CASHIER;
         }
     }
-
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
